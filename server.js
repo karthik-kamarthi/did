e941ec16fb52e0e1354811e7e9161b5d3f552535
karthik <karthik.k6071@gmail.com>
2018-03-13 00:13:53 +0530

    8080 port

--- server.js
+++ server.js
@@ -2,14 +2,14 @@ var express = require('express'),
     app     = express(),
     path =  require('path');
 
-var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
-    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || 'localhost';
+var appPort = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+	ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
 
     var http = require('http')
     , server = http.createServer(app, ip);
 
-    server.listen(port, ip);
+    server.listen(appPort, ip);
 
 
 app.use('/assets', express.static(path.join(__dirname, 'public/assets')));
@@ -21,14 +21,14 @@ app.engine('html', require('hbs').__express);
 
 
 app.get('/', (req, res) => {
-    res.render('./index.html');
+    res.render('index.html');
 });
 
 
 if (process.env) {
     console.log("Nodejs Server listening on port " + process.env.OPENSHIFT_NODEJS_PORT);
-    console.log("IP Server listening on IP " + ip);
+     console.log("IP Server listening on IP " + process.env.OPENSHIFT_NODEJS_IP);
 }
 
-console.log("Nodejs Server listening on port " , port);
+console.log("Nodejs Server listening on port " , appPort);
 console.log("Nodejs Server listening on port " , ip);
